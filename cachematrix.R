ab8f3c352b537299c26a63ea275242c9f477d367
Patrick Kansa <pkansa@gmail.com>
2015-09-22 15:13:55 -0500

    Fulfill assignment
    As requested in the assignment, the existing file has been updated to
    reflect the necessary steps to calculate the inverse of a matrix and
    cache the result.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the 
+## inverse of a matrix rather than computing it repeatedly.  This  pair of functions will cache
+## the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will do the following:
+## 1 - set the value of the matrix
+## 2 - get the value of the matrix
+## 3 - set the value of the inverse of the matrix
+## 4 - get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                inverted <- NULL
+                set <- function(y) {
+                        x <<- y
+                        inverted <<- NULL
+                }
+                get <- function() x
+                setinverse <- function(inverse) inverted <<- inverse
+                getinverse <- function() inverted
+                list(set = set, get = get,
+                     setinverse = setinverse,
+                     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This next function will generate the inverse of a matrix.  It first checks to see if the inverse
+## haS already been generated (and cached).  If it has, it will return it; otherwise, it will run
+## the inverse through, cache it, and return the results
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverted <- x$getinverse()
+        if(!is.null(inverted)) {
+                message("getting cached data")
+                return(inverted)
+        }
+        data <- x$get()
+        inverted <-solve(data, ...)  ## solve is what does the inverting
+        x$setinverse(inverted)
+        inverted
 }
